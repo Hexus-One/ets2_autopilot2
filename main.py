0daa45fc5fcb5ae99eb48d2711074a59f33b0e10
Hexus-One <darnoc2401+github@hotmail.com>
2023-08-11 12:51:43 +1000

    Experiment with thresholding and masking.

--- main.py
+++ main.py
@@ -13,17 +13,24 @@ if __name__ == '__main__':
     # This works for Win10/8 but not 7/Vista
     errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
-    crop_x = 1480
-    crop_y = 747
+    # hardcoded GPS colours, remember its BGRA not RGBA
+    GREEN_MIN = (6, 229, 17, 255)
+    GREEN_MAX = (28, 251, 97, 255)
+    RED_MIN = (11, 11, 194, 255)
+    RED_MAX = (42, 42, 207, 255)
+    KERNEL = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+    CROP_X = 1480
+    CROP_Y = 747
     # Homography from https://learnopencv.com/homography-examples-using-opencv-python-c/
     # Hardcoded points for homography :)
-    pts_src = np.array([[1724-crop_x,883-crop_y],
-                        [1744-crop_x,953-crop_y],
-                        [1637-crop_x,953-crop_y],
-                        [1657-crop_x,883-crop_y]])
-    pts_dst = np.array([[1006,863], [1006,981], [911,981], [911,863]])
-    h, status = cv2.findHomography(pts_src, pts_dst)
-
+    PTS_SRC = np.array([[1724-CROP_X,883-CROP_Y],
+                        [1744-CROP_X,953-CROP_Y],
+                        [1637-CROP_X,953-CROP_Y],
+                        [1657-CROP_X,883-CROP_Y]])
+    PTS_DST = np.array([[1006,863], [1006,981], [911,981], [911,863]])
+    # getPerspectiveTransform would also work here
+    h, status = cv2.findHomography(PTS_SRC, PTS_DST)
     window_handle = FindWindow(None, "Euro Truck Simulator 2")
 
     with mss.mss() as sct:
@@ -32,15 +39,22 @@ if __name__ == '__main__':
             # grab window position
             # assuming you're using Win10 + ETS2 in 1920x1080 window
             window_rect = GetWindowRect(window_handle)
-            ets2_window = {"top": window_rect[1]+crop_y,
-                        "left": window_rect[0]+10+crop_x, # no clue why
-                        "width": 1921-crop_x,
-                        "height": 1120-crop_y
+            ets2_window = {"top": window_rect[1]+CROP_Y,
+                        "left": window_rect[0]+10+CROP_X, # no clue why
+                        "width": 1921-CROP_X,
+                        "height": 1120-CROP_Y
                         }
             im_src = np.array(sct.grab(ets2_window))
-            im_out = cv2.warpPerspective(im_src, h, (1921, 1120))
+            green_mask = cv2.inRange(im_src, GREEN_MIN, GREEN_MAX)
+            red_mask = cv2.inRange(im_src, RED_MIN, RED_MAX)
+            comb_mask = cv2.bitwise_or(green_mask, red_mask)
+            comb_mask = cv2.morphologyEx(comb_mask, cv2.MORPH_CLOSE, KERNEL)
+            comb_mask_inv = cv2.bitwise_not(comb_mask)
+            im_tmp3 = cv2.bitwise_and(im_src, im_src, mask=comb_mask_inv)
+            im_out = cv2.warpPerspective(im_tmp3, h, (1921, 1120), flags=cv2.INTER_NEAREST)
 
             cv2.imshow('Source Image', im_src)
+            cv2.imshow('Mask', comb_mask)
             cv2.imshow("Warped Source Image", im_out)
 
             print(f'FPS: {1/(time.time()-last_time)}')
