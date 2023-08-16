44c20550f8c13552d99b6ecc4547a409af5729a4
Hexus-One <darnoc2401+github@hotmail.com>
2023-08-16 13:00:30 +1000

    Add some hints to calc_input.py

--- ets2_autopilot/calc_input.py
+++ ets2_autopilot/calc_input.py
@@ -1,6 +1,10 @@
 # calc_input.py
 # from telemetry and centreline, calculate what inputs to send
 
+# telemetry avaiable is here: https://github.com/RenCloud/scs-sdk-plugin#telemetry-fields-and-the-c-object
+# centreline scale is 1 unit -> 25cm
+#  (x, y) -> (0, 0) is centre of the cab
+#  +x is left, -x is right, +y is forwards and -y is backwards
 def calc_input(telemetry, centreline):
     steering = 0 # some number between -1, 1 (1 is left, -1 is right)
     throttle = 0 # some integer -1, 0, 1 (-1 is brake, 1 is throttle)
