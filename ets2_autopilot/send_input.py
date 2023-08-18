9d14209096103656901cb0ef2aacb1ec5ec949ac
Hexus-One <darnoc2401+github@hotmail.com>
2023-08-18 23:22:48 +1000

    Implement placeholder send input, confirm input module (pydirectinput) works fine

--- ets2_autopilot/send_input.py
+++ ets2_autopilot/send_input.py
@@ -1,6 +1,25 @@
 # send_input.py
 # send input to ETS2 via keyboard/mouse inputs
+# note: controls.sii in game needs to be changed to:
+#  config_lines[0]: "device keyboard `sys.keyboard`"
+#  config_lines[1]: "device mouse `sys.mouse`"
+
+import pydirectinput
 
 
-def send_input(steering, throttle):
+def send_input(telemetry, steering, throttle):
+    # use relative move to control steering
+    # positive to steer right, negative to steer left
+    steer(10)
+    """
+    if telemetry["InputValues"]["Steering"] > steering:
+        steer(5)
+    else:
+        steer(-5)
+    """
     return
+
+
+# TODO: change this to platform agnostic
+def steer(movement):
+    pydirectinput.move(movement, 0, _pause=False, relative=True)
