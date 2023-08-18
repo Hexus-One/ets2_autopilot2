2fdc29597da22cb203ed87b240b3940d6c0e8555
robustprogram <robustprogram@gmail.com>
2023-08-19 00:46:51 +1000

    Fix bugs with telemetry

--- ets2_telemetry/control_values.py
+++ ets2_telemetry/control_values.py
@@ -5,9 +5,9 @@ def get_input_values(telemetry_memory_map):
     """Get user's input values"""
     return {
         "steer": struct.unpack("f", telemetry_memory_map[776:780])[0],
-        "throttle": struct.unpack("I", telemetry_memory_map[784:788])[0],
-        "brake": struct.unpack("Q", telemetry_memory_map[792:796])[0],
-        "clutch": struct.unpack("Q", telemetry_memory_map[796:800])[0],
+        "throttle": struct.unpack("f", telemetry_memory_map[784:788])[0],
+        "brake": struct.unpack("f", telemetry_memory_map[792:796])[0],
+        "clutch": struct.unpack("f", telemetry_memory_map[796:800])[0],
     }
 
 
@@ -15,9 +15,9 @@ def get_game_values(telemetry_memory_map):
     """Get the game's control values"""
     return {
         "steer": struct.unpack("f", telemetry_memory_map[800:804])[0],
-        "throttle": struct.unpack("I", telemetry_memory_map[804:808])[0],
-        "brake": struct.unpack("Q", telemetry_memory_map[808:812])[0],
-        "clutch": struct.unpack("Q", telemetry_memory_map[812:816])[0],
+        "throttle": struct.unpack("f", telemetry_memory_map[804:808])[0],
+        "brake": struct.unpack("f", telemetry_memory_map[808:812])[0],
+        "clutch": struct.unpack("f", telemetry_memory_map[812:816])[0],
     }
 
 
