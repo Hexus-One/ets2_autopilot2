cf92ef1857aa1ddfaa3252e915a7718d24f348b5
robustprogram <robustprogram@gmail.com>
2023-08-19 00:37:19 +1000

    Add control values

--- ets2_telemetry/__init__.py
+++ ets2_telemetry/__init__.py
@@ -1,6 +1,7 @@
 from mmap import mmap
 
-from telemetry import get_general_info
+from general_info import get_general_info
+from control_values import get_control_values
 
 # Defined by https://github.com/RenCloud/scs-sdk-plugin/blob/master/scs-telemetry/inc/scs-telemetry-common.hpp#L26
 memory_map_length = 1024
@@ -8,3 +9,4 @@ shared_memory_location = "Local\\SCSTelemetry"
 telemetry_memory_map = mmap(0, memory_map_length, shared_memory_location)
 
 general_info = get_general_info(telemetry_memory_map)
+control_values = get_control_values(telemetry_memory_map)
