7dcf34d8521cce078179c4ed0fadc3b76bdc67e0
robustprogram <robustprogram@gmail.com>
2023-08-19 01:08:25 +1000

    Fix incorrect offsets

--- ets2_telemetry/control_values.py
+++ ets2_telemetry/control_values.py
@@ -6,18 +6,18 @@ def get_input_values(telemetry_memory_map):
     return {
         "steer": struct.unpack("f", telemetry_memory_map[952:956])[0],
         "throttle": struct.unpack("f", telemetry_memory_map[956:960])[0],
-        "brake": struct.unpack("f", telemetry_memory_map[964:968])[0],
-        "clutch": struct.unpack("f", telemetry_memory_map[972:976])[0],
+        "brake": struct.unpack("f", telemetry_memory_map[960:964])[0],
+        "clutch": struct.unpack("f", telemetry_memory_map[964:968])[0],
     }
 
 
 def get_game_values(telemetry_memory_map):
     """Get the game's control values"""
     return {
-        "steer": struct.unpack("f", telemetry_memory_map[976:980])[0],
-        "throttle": struct.unpack("f", telemetry_memory_map[980:984])[0],
-        "brake": struct.unpack("f", telemetry_memory_map[984:988])[0],
-        "clutch": struct.unpack("f", telemetry_memory_map[988:992])[0],
+        "steer": struct.unpack("f", telemetry_memory_map[968:972])[0],
+        "throttle": struct.unpack("f", telemetry_memory_map[972:976])[0],
+        "brake": struct.unpack("f", telemetry_memory_map[980:984])[0],
+        "clutch": struct.unpack("f", telemetry_memory_map[984:998])[0],
     }
 
 
